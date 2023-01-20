08dae2e43b83e719ad0f9658db11eca9f2c4be82
Tomal <applesurface@Apples-MacBook-Pro.local>
2023-01-20 23:43:08 +0600

    New Code Push

--- template-1/views/index.view.php
+++ template-1/views/index.view.php
@@ -9,7 +9,7 @@
         <!-- Replace with your content -->
         <div class="px-4 py-6 sm:px-0">
             <div class="h-96 rounded-lg border-4 border-dashed border-gray-200">
-                <h2 class="text-4xl text-dark font-bold p-5">This is <?= "$heading" ?> Page</h2>
+                <h2 class="text-4xl text-dark font-bold p-5">This is Home Page</h2>
             </div>
         </div>
         <!-- /End replace -->
